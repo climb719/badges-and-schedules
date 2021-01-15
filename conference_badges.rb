575b590b34d1fbabb0b3b017cdff7721358e287c
climb719 <mary.oldweiler@gmail.com>
2021-01-15 06:03:21 -0500

    commit tree 36cf4ce8df6d0cdbc2edb7e5da3a02e6c7555920

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    new_array = []
+    names.each do |name| 
+    new_array << badge_maker(name)
+  end
+   return new_array
+end
+
+def assign_rooms(names)
+    assignments = []
+    names.each_with_index do |names, index| 
+        assignments << "Hello, #{names}! You'll be assigned to room #{index+1}!"
+    end
+   assignments
+end
+
+def printer(names)
+    batch_badge_creator(names).each{ |badges| puts badges}
+    assign_rooms(names).each{ |rooms| puts rooms}
+end
+
+
+
+
+
